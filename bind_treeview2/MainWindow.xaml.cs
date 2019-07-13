c0ee381ccb93795f099fc7f5d31fb5f917f93e07
szpgpp <szpgpp@qq.com>
2019-07-13 11:30:04 +0800

    bind treeview

---
+++ bind_treeview2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace bind_treeview2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private XmlElement XES = null;
+        private XmlDataProvider XDP = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            doc.AppendChild(dec);
+
+            XmlElement xeRoot = doc.CreateElement("Root");
+            {
+                XmlElement xeStudents = doc.CreateElement("Student");                
+                xeStudents.SetAttribute("Name", "Students");
+                xeStudents.SetAttribute("IsSelected", "True");
+                XES = xeStudents;
+                {
+                    XmlElement xeStudent = doc.CreateElement("Student");
+                    xeStudent.SetAttribute("Name", "Szp");
+                    xeStudents.AppendChild(xeStudent);
+                }
+                {
+                    XmlElement xeStudent = doc.CreateElement("Student");
+                    xeStudent.SetAttribute("Name", "Szh");
+                    xeStudents.AppendChild(xeStudent);
+                }
+                xeRoot.AppendChild(xeStudents);
+            }
+            doc.AppendChild(xeRoot);
+
+            XmlDataProvider provider = new XmlDataProvider
+            {
+                Document = doc,
+                XPath = "Root/Student"
+            };
+            this.XDP = provider;
+            this.XDP.Document.NodeChanged += (sender1, e1) => { this.XDP.Refresh(); };
+
+            //this.TV1.Items.Clear();
+            this.TV1.DataContext = provider;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var provider = this.TV1.DataContext as XmlDataProvider;
+            var xmldoc = provider.Document;
+            var students = xmldoc.SelectSingleNode("Root/Student") as XmlElement;
+            students.SetAttribute("IsExpanded","1");
+            this.XDP.Refresh();
+        }
+    }
+}
---
+++ bind_treeview3/Class1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bind_treeview3
+{
+    public class BaseNotifyPropertyChanged : System.ComponentModel.INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 属性值变化时发生
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 属性值变化时发生
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = (propertyExpression.Body as MemberExpression).Member.Name;
+            this.OnPropertyChanged(propertyName);
+        }
+
+        public virtual event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    }
+    class DataItem : BaseNotifyPropertyChanged, ICloneable
+    {
+        public DataItem(string header, int deepth = 1)
+        {
+            Header = header;
+            Deepth = deepth;
+        }
+
+        public object Clone()
+        {
+            DataItem dataItem = new DataItem(Header, Deepth);
+            dataItem.IsExpanded = IsExpanded;
+            dataItem.IsSelected = IsSelected;
+            dataItem.Deepth = Deepth;
+            foreach (DataItem item in Items)
+                dataItem.Items.Add((DataItem)item.Clone());
+            return dataItem;
+        }
+
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                 this.header = value; this.OnPropertyChanged("Header"); 
+            }
+        }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { this.isSelected = value;this.OnPropertyChanged("IsSelected"); }
+        }
+
+        private bool isExpanded;
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set { this.isExpanded = value; this.OnPropertyChanged("IsExpanded"); }
+        }
+
+        private int deepth;
+        public int Deepth
+        {
+            get { return deepth; }
+            set { this.deepth = value; this.OnPropertyChanged("Deepth"); }
+        }
+
+        private ObservableCollection<DataItem> mItems = null;
+        public ObservableCollection<DataItem> Items
+        {
+            get
+            {
+                if (mItems == null)
+                    mItems = new ObservableCollection<DataItem>();
+                return mItems;
+            }
+        }
+
+    }
+
+    class Data
+    {
+        private static Data mInstance = new Data();
+
+        public static Data Instance
+        {
+            get { return mInstance; }
+        }
+
+        private ObservableCollection<DataItem> GenerateTreeViewItems()
+        {
+            ObservableCollection<DataItem> items = new ObservableCollection<DataItem>();
+
+            DataItem item1 = new DataItem("TreeViewItem1");
+            item1.Items.Add(new DataItem("SubItem1", item1.Deepth));
+            item1.Items.Add(new DataItem("SubItem2", item1.Deepth));
+            item1.Items.Add(new DataItem("SubItem3", item1.Deepth));
+            item1.Items.Add(new DataItem("SubItem4", item1.Deepth));
+            items.Add(item1);
+
+            DataItem item2 = new DataItem("TreeViewItem2");
+            item2.Items.Add(new DataItem("SubItem1", item2.Deepth));
+            item2.Items.Add(new DataItem("SubItem2", item2.Deepth));
+            items.Add(item2);
+
+            DataItem item3 = new DataItem("TreeViewItem3");
+            item3.IsExpanded = true;
+            item3.Items.Add(new DataItem("SubItem1", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem2", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem3", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem4", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem5", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem6", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem7", item3.Deepth));
+            item3.Items.Add(new DataItem("SubItem8", item3.Deepth));
+            items.Add(item3);
+
+            return items;
+        }
+
+        private ObservableCollection<DataItem> GenerateListItems()
+        {
+            ObservableCollection<DataItem> items = new ObservableCollection<DataItem>();
+            items.Add(new DataItem("ListBoxItem1"));
+            items.Add(new DataItem("ListBoxItem2"));
+            items.Add(new DataItem("ListBoxItem3"));
+            items.Add(new DataItem("ListBoxItem4"));
+            items.Add(new DataItem("ListBoxItem5"));
+            return items;
+        }
+
+        public ObservableCollection<DataItem> TreeViewItems
+        {
+            get
+            {
+                if (mTreeViewItems == null)
+                    mTreeViewItems = GenerateTreeViewItems();
+                return mTreeViewItems;
+            }
+        }
+
+        public ObservableCollection<DataItem> ListBoxItems
+        {
+            get
+            {
+                if (mListBoxItems == null)
+                    mListBoxItems = GenerateListItems();
+                return mListBoxItems;
+            }
+        }
+
+        private ObservableCollection<DataItem> mTreeViewItems = null;
+        private ObservableCollection<DataItem> mListBoxItems = null;
+    }
+}
